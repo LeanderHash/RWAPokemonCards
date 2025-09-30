ea3d4b8943ab3d3d1e2132917dbe5b670e2ec504
algernonx <kisnexmv9496@hotmail.com>
2025-09-30 14:59:43 +0800

    update react

--- home/vite.config.ts
+++ home/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    fallback: {
-      buffer: require.resolve('buffer/'),
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      path: require.resolve('path-browserify'),
-    },
-  },
-  define: {
-    global: 'globalThis',
-  },
 })
